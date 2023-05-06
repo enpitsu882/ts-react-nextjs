63765dc0c5347fdcc93114d2d6d7358f217c259c
enpitsu882 <enpitsu882@hotmail.co.jp>
2023-05-06 23:58:26 +0900

    obj型まで

--- ts-intro/sayHello.js
+++ ts-intro/sayHello.js
@@ -1,5 +1,20 @@
+"use strict";
 function sayHello(firstName) {
     console.log('Hello ' + firstName);
 }
-var firstName = 'Takuya';
+let firstName = 'Takuya';
 sayHello(firstName);
+// 配列
+const mixedArray = ['foo', 1];
+const mixedArrayU = ['foo', 1]; // Union型
+const mixedArrayT = ['foo', 1]; // タプル
+// オブジェクト型
+const user = {
+    name: 'Takuya',
+    age: 36
+};
+function printName(obj) {
+    console.log(obj);
+}
+printName({ firstName: 'Takuya' });
+printName({ firstName: 'Takuya', lastName: 'Tejima' });
--- ts-intro/sayHello.ts
+++ ts-intro/sayHello.ts
@@ -4,3 +4,21 @@ function sayHello (firstName: string) {
 
 let firstName: string = 'Takuya';
 sayHello(firstName);
+
+// 配列
+const mixedArray = ['foo', 1];
+const mixedArrayU: (string|number)[] = ['foo', 1]; // Union型
+const mixedArrayT: [string, number] = ['foo', 1]; // タプル
+
+// オブジェクト型(プロパティに?を付けると省略可となる)
+const user: { name: string; age: number } = {
+  name: 'Takuya',
+  age: 36
+};
+function printName(obj: { firstName: string; lastName?: string }) {
+  console.log(obj);  
+}
+printName({ firstName: 'Takuya' });
+printName({ firstName: 'Takuya', lastName: 'Tejima' });
+
+// p.44まで
